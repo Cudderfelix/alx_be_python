5109c26a270a554a7d2b8ad3c0317b38f586457d
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-13 22:36:12 +0100

    Simple interest calc using python

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000 # This is representing 1000 dollars
+rate = 0.05 # This represents 5% annual interest rate
+time = 3 # This represents the amount of tenor years
+interest = principal * rate * time
+print ("The simple interest is: ", interest)
