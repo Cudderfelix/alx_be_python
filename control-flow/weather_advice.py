9fe81b4de97852c50a95f685d4bd724d79047925
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-20 19:09:07 +0100

    alx-weather-conditionals

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,5 @@
 weather = "sunny", "rainy", "cold"
-get_weather_advice = str(input("Enter the weather condition (sunny, rainy, cold): "))
+get_weather_advice = str(input("What's the weather like today? (sunny/rainy/cold): "))
 if get_weather_advice == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif get_weather_advice == "rainy":
@@ -7,5 +7,5 @@ elif get_weather_advice == "rainy":
 elif get_weather_advice == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Invalid weather condition entered. Please try again.")
-print("Thank you for using the weather advice program!")    
+    print("Sorry I don't have recommendations for this weather.")
+print("Thank you for using the weather advice program!")  
