448312238fd4f41883c2d969cb112f51e9d8a985
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-20 19:14:14 +0100

    alx-weather-conditionals

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,10 @@
 weather = "sunny", "rainy", "cold"
-get_weather_advice = str(input("What's the weather like today? (sunny/rainy/cold): "))
-if get_weather_advice = "sunny":
+weather_advice = str(input("What's the weather like today? (sunny/rainy/cold): "))
+if weather_advice == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif get_weather_advice = "rainy":
+elif weather_advice == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif get_weather_advice = "cold":
+elif weather_advice == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry I don't have recommendations for this weather.")
-print("Thank you for using the weather advice program!")
