25054b05211a39f0a5c5160faffe5d1a3ffbfe4f
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-24 18:16:23 +0100

    multiplication_play_book

---
+++ python_introduction/multiplication_play.py
@@ -0,0 +1,5 @@
+num1 = int(input("Please input your first number:"))
+num2 = int(input("Please input your second number:"))
+multiplication_sequence = num1 * num2
+print ("The multiplication", "of", num1, "and", num2, "is", multiplication_sequence,".")
+
