018fd87bc7965f54f72cc187f2d411f69dc46790
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-25 09:00:58 +0100

    lambada_test

--- lambda_test.py
+++ lambda_test.py
@@ -8,3 +8,14 @@ def increment_global():
     count += 1
 increment_global()
 print(count) #output: 1 (Global count is modified)
+
+def outer_function():
+    x = 10 #Variable in the enclosing function
+
+    def inner_function():
+        nonlocal x #Using nonlocal to modify x from the enclosing function
+        x += 5 #Modifying the value of x
+
+        inner_function() # Calling the nested function
+        print("Modified value of x from inner function:", x)
+outer_function()
