427f2af52a420f19cab0cf809bca590fc3d1bde8
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-25 08:48:28 +0100

    lambada_test

--- lambda_test.py
+++ lambda_test.py
@@ -2,3 +2,9 @@ add = lambda x, y: x + y
 result = add(5, 3)
 print(result) #Output: 8
 
+count = 0
+def increment_global():
+    global count 
+    count += 1
+increment_global()
+print(count) #output: 1 (Global count is modified)
