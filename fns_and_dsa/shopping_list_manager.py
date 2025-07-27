85d64902e1f0da21b8627e291d456fb58646c2d6
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-27 12:38:23 +0100

    shopping_list_error-corrected1.1

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,7 +10,7 @@ def main():
     while True:
         display_menu()
         choice = input("Enter your choice: ").strip()
-        if choice.isdigit() and choice in ['1, 2', '3', '4']:
+        if choice.isdigit() and choice in [1, 2, 3, 4]:
             if choice == 1:
              item = input ("Enter the item to add: ").strip()
             if item:
