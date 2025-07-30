d31185f4695d06b1898b1f8d49b7a0fba85e7389
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-30 15:19:41 +0100

    practising_inheritance_polymorphism

--- fns_and_dsa/dog_class.py
+++ fns_and_dsa/dog_class.py
@@ -15,3 +15,25 @@ dog2 = Dog ("Musa", "long", "tall")
 dog3 = Dog ("Abacha", "medium", "short")
 
 print(f"{dog1.name} has a {dog1.fur_length} fur and {dog1.height} height. with a barking sound of {dog1.barking_sound("lala!")} and speed of {dog1.speed("30km/h")}.")
+
+# Real World Example #Inheritance
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        pass
+class Lion(Animal):
+    def speak(self):
+        return f"{self.name} roars!"
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} barks!"   
+class Elephant(Animal):
+    def speak(self):
+        return f"{self.name} trumpets!"     
+
+print(f"{Lion('Simba').speak()}")
+print(f"{Dog('Chiwendu').speak()}")
+print(f"{Elephant("Dumbo").speak()}")
+
+# Polymorphism Example
