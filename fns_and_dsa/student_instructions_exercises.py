e50c975849629175eff73a24fb91749ca8512ab7
Cudderfelix <ifeachochukafelix@gmail.com>
2025-07-30 16:43:04 +0100

    playing_around_with_python

---
+++ fns_and_dsa/student_instructions_exercises.py
@@ -0,0 +1,7 @@
+class student:
+    def _init_ (self, name, age):
+        self.name = name
+        self.age = age
+    def display_info(self):
+        return f"Name: {self.name}, Age: {self.age}"
+              
