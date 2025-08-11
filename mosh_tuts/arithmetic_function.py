383d3cbde1c0ec8097091b7caaa9d2a984357226
Cudderfelix <ifeachochukafelix@gmail.com>
2025-08-11 13:35:04 +0100

    addition_commit

---
+++ mosh_tuts/arithmetic_function.py
@@ -0,0 +1,5 @@
+first = input("Enter first number: ")
+second = input("Enter second number: ")
+
+total_sum = int(first) + int(second)
+print("The sum of the two numbers is: ", "First:", first, "Second:", second)
